b028f07544a0e1aecc6912ed46ead00ee99ebeb2
Roché <rocherfourie@gmail.com>
2026-02-18 22:48:39 +0200

    Updated paths

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+app.get("/healthz", handlerReadiness);
+function handlerReadiness(req, res) {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send("OK");
+}
